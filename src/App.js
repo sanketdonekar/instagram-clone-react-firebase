f768b51992e80b280d343770aebe9c135f1ebdcc
sanketdonekar <sanket.d.mj@gmail.com>
2020-08-15 02:42:52 +0530

    signup auth

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from "react";
 import "./assets/css/App.css";
 import Post from './components/post.jsx'
 import { db } from './firebase';
-import { makeStyles, Button } from "@material-ui/core";
+import { makeStyles, Button, Input } from "@material-ui/core";
 import Modal from '@material-ui/core/Modal'
+import { auth } from "firebase";
+
 
 //useEffects : runs a piece of code based on a specific condition
 
@@ -31,15 +33,50 @@ const useStyle = makeStyles((theme) => ({
 }))
 
 
-const signUp = (event) => {
-
-}
-
 function App() {
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
   const classes = useStyle();
   const [modalStyle] = useState(getModelStyle);
+
+  const [user, setUser] = useState(null);
+
+
+  useEffect(() => {
+    const unsubscribe = auth().onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //user has logged in...
+        console.log(user);
+        setUser(authUser);
+      } else {
+        // user has logged out
+        setUser(null);
+      }
+    })
+
+    return () => {
+      // perform some cleanup actions 
+      unsubscribe();
+    }
+
+  }, [user, username]);
+
+  const signUp = (event) => {
+    event.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username
+        })
+      })
+      .catch((error) => alert(error.message));
+  }
 
   useEffect(() => {
     // this is were the code runs and using [] makes it work for single time & onSnapshot is powerfull listner that watches new doc in firebase db 
@@ -54,16 +91,17 @@ function App() {
 
   return (
     <div className="App">
-
-      <Modal
-        open={open}
-        onClose={() => setOpen(false)}
-
-      >
+      <Modal open={open} onClose={() => setOpen(false)}>
         <div style={modalStyle} className={classes.paper}>
-          <center>
-            <img className="app-headerImage" src={require('./assets/img/instagram-logo.png')} alt="" />
-          </center>
+          <form className="app-signup">
+            <center>
+              <img className="app-headerImage" src={require('./assets/img/instagram-logo.png')} alt="" />
+            </center>
+            <Input type="text" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <Input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <Input type="text" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <Button type="submit" onClick={signUp}>Sigh Up</Button>
+          </form>
         </div>
       </Modal>
       <div className="app-header">
